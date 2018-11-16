0b9422b0c9d2c80e4578ce0fc2fc5691ee1f7015
nathanbuckley <nth.buckley@gmail.com>
2018-11-16 11:57:59 +0000

    +

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   mode: 'production',
+  devtool: 'source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+      parallel: true,
+      extractComments: true,
+      uglifyOptions: {
+        mangle: true,
+        keep_fnames: true
+        }
+      })
+    ]
   },
   module: {
     rules: [
